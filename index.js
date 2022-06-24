1a56ac633b88488c063251d861ed17ce234b6ee8
Blessinggold <blessingobie36@gmail.com>
2022-06-24 21:38:05 +0100

    first zuri  task push to github

---
+++ index.js
@@ -0,0 +1,10 @@
+const myStatus = 
+    {
+        Name: "Blessing Obie",
+        height: "6ft tall",
+        country: "Nigeria"
+    };
+    
+
+document.getElementById("root").innerHTML =myStatus.Name + " ," + myStatus.height + ", " + myStatus.country 
+
